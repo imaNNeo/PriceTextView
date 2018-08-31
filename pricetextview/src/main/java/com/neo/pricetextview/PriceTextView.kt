3fe35ed8164168e95d22ba18031fd74ca582143c
imaNNeoFighT <iman.neofight@gmail.com>
2018-09-01 00:42:57 +0430

    added PriceTextView class

---
+++ pricetextview/src/main/java/com/neo/pricetextview/PriceTextView.kt
@@ -0,0 +1,16 @@
+package com.neo.pricetextview
+
+import android.content.Context
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+
+/**
+ * Created by iman.
+ * iman.neofight@gmail.com
+ */
+
+class PriceTextView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+}
