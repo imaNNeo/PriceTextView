ab82a410d964dc58156823504987cf9bf1e11cfd
imaNNeoFighT <iman.neofight@gmail.com>
2018-09-26 23:53:01 +0330

    added numbers size animation

--- app/src/main/java/com/neo/pricetextviewdemo/MainActivity.kt
+++ app/src/main/java/com/neo/pricetextviewdemo/MainActivity.kt
@@ -10,6 +10,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        priceTextView.number = 1234
+
+        btn_1.setOnClickListener { onNumberClicked(1) }
+        btn_2.setOnClickListener { onNumberClicked(2) }
+        btn_3.setOnClickListener { onNumberClicked(3) }
+        btn_4.setOnClickListener { onNumberClicked(4) }
+        btn_5.setOnClickListener { onNumberClicked(5) }
+        btn_6.setOnClickListener { onNumberClicked(6) }
+        btn_7.setOnClickListener { onNumberClicked(7) }
+        btn_8.setOnClickListener { onNumberClicked(8) }
+        btn_9.setOnClickListener { onNumberClicked(9) }
+        btn_0.setOnClickListener { onNumberClicked(0) }
+        btn_back.setOnClickListener { onDeleteClicked() }
+    }
+
+    private fun onNumberClicked(i: Int) {
+        priceTextView.number += i.toString()
+    }
+
+    private fun onDeleteClicked() {
+        if (priceTextView.number.isEmpty()) {
+            return
+        }
+
+        priceTextView.number = priceTextView.number.dropLast(1)
     }
 }
--- pricetextview/src/main/java/com/neo/pricetextview/PriceTextView.kt
+++ pricetextview/src/main/java/com/neo/pricetextview/PriceTextView.kt
@@ -1,14 +1,15 @@
 package com.neo.pricetextview
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
-import android.provider.CalendarContract
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 
 /**
  * Created by iman.
@@ -16,127 +17,178 @@ import android.view.View
  */
 
 class PriceTextView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var number: Long = 0
-        set(value) {
-            field = value
-            numberChars = value.toString().toCharArray()
-            requestLayout()
+  var number: String = "0"
+    set(value) {
+      field = if (value.isEmpty()) {
+        "0"
+      } else {
+        if (value.length <= maxChars) {
+          value
+        } else {
+          field
         }
-
-    var numberChars: CharArray = charArrayOf()
-
-
-    var textSpace: Float
-
-    private val textPaint: TextPaint
-
-    private val tmpRect1 = Rect()
-    private val tmpRect2 = Rect()
-
-
-    private val horizontalPadding
-        get() = paddingLeft + paddingRight
-
-    private val verticalPadding
-        get() = paddingTop + paddingBottom
-
-    init {
-        textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
-            color = Color.RED
-            textSize = 28.sp.toFloat()
-        }
-
-        textSpace = 16.dp.toFloat()
+      }
+      numberChars = field.toCharArray()
+      animateSize()
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val resolvedWidth = resolveSize(desireWidth().toInt(), widthMeasureSpec)
-        val resolvedHeight = resolveSize(desireHeight().toInt(), heightMeasureSpec)
+  var numberChars: CharArray = charArrayOf()
 
-        setMeasuredDimension(resolvedWidth, resolvedHeight)
+  var textSpace: Float
+
+  var maxChars : Int
+
+  var maxTextSize = 100.sp.toFloat()
+  var minTextSize = 24.sp.toFloat()
+
+  private val textPaint: TextPaint
+
+  private val tmpRect1 = Rect()
+  private val tmpRect2 = Rect()
+
+  private val horizontalPadding
+    get() = paddingLeft + paddingRight
+
+  private val verticalPadding
+    get() = paddingTop + paddingBottom
+
+  init {
+    textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
+      color = Color.BLACK
+      textSize = maxTextSize
     }
 
-    private fun desireWidth(): Float {
-        val numberDesireWidth = getRequiredTextWidth(numberChars, textPaint)
-        return numberDesireWidth + horizontalPadding
+    textSpace = 16.dp.toFloat()
+
+    number = "0"
+
+    maxChars = 8
+  }
+
+  private fun calculateTextSize(text : String) : Float {
+    return when {
+      text.length < 3 -> maxTextSize
+      text.length < 4 -> ((maxTextSize - minTextSize) * 0.9 + minTextSize).toFloat()
+      text.length < 5 -> ((maxTextSize - minTextSize) * 0.7 + minTextSize).toFloat()
+      text.length < 6 -> ((maxTextSize - minTextSize) * 0.5 + minTextSize).toFloat()
+      text.length < 7 -> ((maxTextSize - minTextSize) * 0.3 + minTextSize).toFloat()
+      text.length < 8 -> ((maxTextSize - minTextSize) * 0.1 + minTextSize).toFloat()
+      text.length < 9 -> minTextSize
+      else -> maxTextSize
     }
+  }
 
-    private fun desireHeight(): Float {
-        val height = getRequiredTextHeight(numberChars, textPaint)
-        return height + verticalPadding
+  private fun animateSize() {
+    val newTextSize = calculateTextSize(number)
+
+    val animatior = ValueAnimator.ofFloat(textPaint.textSize, newTextSize)
+    animatior.addUpdateListener {
+      textPaint.textSize = it.animatedValue as Float
+      invalidate()
     }
+    animatior.interpolator = LinearInterpolator()
+    animatior.start()
+  }
 
+  override fun onMeasure(
+    widthMeasureSpec: Int,
+    heightMeasureSpec: Int
+  ) {
+    val resolvedWidth = resolveSize(desireWidth().toInt(), widthMeasureSpec)
+    val resolvedHeight = resolveSize(desireHeight().toInt(), heightMeasureSpec)
 
-    override fun onDraw(canvas: Canvas?) {
+    setMeasuredDimension(resolvedWidth, resolvedHeight)
+  }
 
-        drawText(canvas)
+  private fun desireWidth(): Float {
+    val numberDesireWidth = getRequiredTextWidth(numberChars, textPaint)
+    return numberDesireWidth + horizontalPadding
+  }
+
+  private fun desireHeight(): Float {
+    val height = getRequiredTextHeight(numberChars, textPaint)
+    return height + verticalPadding
+  }
+
+  override fun onDraw(canvas: Canvas?) {
+
+    drawText(canvas)
+  }
+
+  private fun getRequiredTextWidth(
+    char: Char,
+    textPaint: TextPaint
+  ): Float {
+    return textPaint.measureText(char.toString())
+  }
+
+  private fun getRequiredTextWidth(
+    chars: CharArray,
+    textPaint: TextPaint
+  ): Float {
+    var requiredWidth = 0f
+    for (c in chars) {
+      requiredWidth += getRequiredTextWidth(c, textPaint) + textSpace.toInt()
     }
+    requiredWidth -= textSpace.toInt()
+    return requiredWidth
+  }
 
+  private fun getRequiredTextHeight(
+    char: Char,
+    textPaint: TextPaint
+  ): Float {
+    textPaint.getTextBounds(char.toString(), 0, char.toString().length, tmpRect1)
+    return tmpRect1.height()
+        .toFloat()
+  }
 
-    private fun getRequiredTextWidth(char: Char, textPaint: TextPaint): Float {
-        return textPaint.measureText(char.toString())
+  private fun getRequiredTextHeight(
+    chars: CharArray,
+    textPaint: TextPaint
+  ): Float {
+    var maxHeight = 0f
+    for (c in chars) {
+      val h = getRequiredTextHeight(c, textPaint)
+      if (h > maxHeight) {
+        maxHeight = h
+      }
     }
+    return maxHeight
+  }
 
-    private fun getRequiredTextWidth(chars: CharArray, textPaint: TextPaint): Float {
-        var requiredWidth = 0f
-        for (c in chars) {
-            requiredWidth += getRequiredTextWidth(c, textPaint) + textSpace.toInt()
-        }
-        requiredWidth -= textSpace.toInt()
-        return requiredWidth
+  private fun drawText(canvas: Canvas?) {
+
+    val requiredWidth = getRequiredTextWidth(numberChars, textPaint).toInt()
+    val requiredHeight = getRequiredTextHeight(numberChars, textPaint).toInt()
+
+    //Drawing Bound
+    tmpRect1.set(
+        (width / 2) - (requiredWidth / 2),
+        (height / 2) - (requiredHeight / 2),
+        (width / 2) + (requiredWidth / 2),
+        (height / 2) + (requiredHeight / 2)
+    )
+
+    for (i in 0 until numberChars.size) {
+
+      val char = numberChars[i]
+
+      val w = getRequiredTextWidth(char, textPaint);
+      //Assigned space
+      tmpRect2.set(
+          (tmpRect1.left + (i * w) + (i * textSpace)).toInt(),
+          tmpRect1.top,
+          (tmpRect1.left + (i * w) + (i * textSpace) + w).toInt(),
+          tmpRect1.bottom
+      )
+
+      canvas?.drawText(char.toString(), tmpRect2.left.toFloat(), tmpRect2.bottom.toFloat(), textPaint)
     }
-
-
-    private fun getRequiredTextHeight(char: Char, textPaint: TextPaint): Float {
-        textPaint.getTextBounds(char.toString(), 0, char.toString().length, tmpRect1)
-        return tmpRect1.height().toFloat()
-    }
-
-    private fun getRequiredTextHeight(chars: CharArray, textPaint: TextPaint): Float {
-        var maxHeight = 0f
-        for (c in chars) {
-            val h = getRequiredTextHeight(c, textPaint)
-            if (h > maxHeight) {
-                maxHeight = h
-            }
-        }
-        return maxHeight
-    }
-
-
-    private fun drawText(canvas: Canvas?) {
-
-        val requiredWidth = getRequiredTextWidth(numberChars, textPaint).toInt()
-        val requiredHeight = getRequiredTextHeight(numberChars, textPaint).toInt()
-
-        //Drawing Bound
-        tmpRect1.set(
-                (width / 2) - (requiredWidth / 2),
-                (height / 2) - (requiredHeight / 2),
-                (width / 2) + (requiredWidth / 2),
-                (height / 2) + (requiredHeight / 2))
-
-        val p = Paint()
-        p.color = Color.GREEN
-        canvas?.drawRect(tmpRect1, p)
-
-        for (i in 0 until numberChars.size) {
-
-            val char = numberChars[i]
-
-            val w = getRequiredTextWidth(char, textPaint);
-            //Assigned space
-            tmpRect2.set(
-                    (tmpRect1.left + (i * w) + (i * textSpace)).toInt(),
-                    tmpRect1.top,
-                    (tmpRect1.left + (i * w) + (i * textSpace) + w).toInt(),
-                    tmpRect1.bottom
-                    )
-
-            canvas?.drawText(char.toString(), tmpRect2.left.toFloat(), tmpRect2.bottom.toFloat(), textPaint)
-        }
-    }
+  }
 }
